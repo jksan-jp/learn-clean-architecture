48e2d9c2ed1db7e25c2c9c4fa35538e264f49d47
jksan <jksan.jp@gmail.com>
2023-12-10 11:08:15 +0900

    add usecase user_interactor

---
+++ app/usecase/user_interactor.go
@@ -0,0 +1,12 @@
+package usecase
+
+import "learn-clean-architecture/domain"
+
+type UserInteractor struct {
+	UserRepository UserRepository
+}
+
+func (interactor *UserInteractor) Add(u domain.User) (err error) {
+	_, err = interactor.UserRepository.Store(u)
+	return
+}
